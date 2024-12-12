4dba220fb74b8be4a5a3ae6b4c2c49db96c642b3
David Revillot <drevillotna@gmail.com>
2024-12-12 16:57:13 -0300

    Fix image header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
+import headerImage from '../assets/Header.jpg';  // Asegúrate de que la ruta sea correcta
 
 const Header = () => {
   return (
     <>
     <Card style={{ border: '0px'}} >
-        <Card.Img variant="top" src="src/assets/Header.jpg" style={{ height: '200px', objectFit: 'cover', filter:'brightness(0.3)' }} />
+    <Card.Img variant="top" src={headerImage} style={{ height: '200px', objectFit: 'cover', filter:'brightness(0.3)' }} />
         <Card.ImgOverlay className="d-flex align-items-center justify-content-center">
             <Card.Title className="text-white text-center">
             <h1>¡Pizzería Mamma Mia!</h1>
